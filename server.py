2eeb0eba7067ae0362a3ae01723c8d29a3d7973b
Liam Charpentier <liamgen.js@proton.me>
2024-12-11 10:36:35 +0100

    Update server.py

--- server.py
+++ server.py
@@ -4,29 +4,55 @@ import json
 import multiprocessing
 
 app = Flask(__name__)
-values = {}
 
 @app.route("/")
 def hello_world():
     return render_template('index.html')
 
-@app.route("/api/data/:id")
-def get_data(capteur):
-    return jsonify(values[capteur])
+@app.get("/api/data/<id>")
+def get_data(id):
+    f = open("data.json", "r")
+    current_data = json.loads(f.read())
+    f.close()
+    return jsonify(current_data[id])
 
 
 def runApp():
     app.run(port=8080)
 
 def runGetData():
-    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
     ser.reset_input_buffer()
     while True:
         if ser.in_waiting > 0:
-            line = ser.readline().decode('utf-8').rstrip()
-            data = json.loads(line)
-            if(not data["error"]):
-                values[data["type"]] = data["value"]
+            #print(ser.readline())
+            try:
+                line = ser.readline().decode('utf-8').rstrip()
+            except UnicodeDecodeError:
+                line = ser.readline().decode('utf-8').rstrip()
+            try:
+                data = json.loads(line)
+                if(not data["error"]):
+                    f = open("data.json", "r")
+                    
+                    # Créer si il est vide
+                    if(f.read()) == "":
+                        f.close()
+                        f2 = open("data.json", "w")
+                        json.dump("{}", f2)
+                        f2.close()
+                        current_data = {}
+                    else:
+                        current_data = json.loads(f.read())
+                        f.close()
+                    
+                    current_data[data["type"]] = data["value"]
+                    f2 = open("data.json", "w")
+                    json.dump(current_data, f2)
+                    f2.close()
+                    
+            except ValueError as e:
+                print("err: "+str(e))
 
 if __name__ == "__main__":
 
@@ -38,5 +64,3 @@ if __name__ == "__main__":
 
     p1.join()
     p2.join()
-
-    print("Done!")
